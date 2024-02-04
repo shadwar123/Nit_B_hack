d70500194785cc1d416da40ddc61226ea0b658ba
shadwar123 <shadwarnayyar@gmail.com>
2024-02-04 06:51:07 +0530

    Chatbot added

--- frontend/src/Components/Chartbot.js
+++ frontend/src/Components/Chartbot.js
@@ -5,7 +5,7 @@ function Chartbot() {
         <div>
             <h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Chat Bot&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h2>
           <iframe
-            src="http://192.168.98.204:8501/"
+            src="https://chatbot-blind.streamlit.app/"
             width="100%"
             height="600px"
             title="Streamlit App"
